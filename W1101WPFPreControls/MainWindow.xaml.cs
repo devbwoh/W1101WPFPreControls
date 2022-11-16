8f4a84e597977422f51a4b06222c6a693390699a
BWOH <bwoh@outlook.kr>
2022-11-17 00:51:00 +0900

    초기 커밋.

---
+++ W1101WPFPreControls/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace W1101WPFPreControls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            string str = $"{Singer.Text}{(HasJongsung(Singer.Text) ? "이" : "가")} 부른 "
+                + $"{Genre.Text} 장르의 "
+                + $"{Title.Text}{ (HasJongsung(Title.Text) ? "이" : "가")} 추가됨\n";
+            //Result.Text += str;
+            Result.Text = str + Result.Text;
+        }
+
+        public bool HasJongsung(string str)
+        {
+            if (str.Length < 1)
+                return true;
+            char last = str[str.Length - 1];
+            return (last - 44032) % 28 != 0 ? true : false;
+        }
+    }
+}
